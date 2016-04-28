762ba1bcf86ffb9f5db7a63e771b72411dc5e0fe
frostkom <martin.fors@frostkom.se>
2016-04-28 09:53:01 +0200

    Add flex CSS for webkit and ms

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Email
 Plugin URI: https://github.com/frostkom/mf_email
 Description: 
-Version: 3.3.9
+Version: 3.3.10
 Author: Martin Fors
 Author URI: http://frostkom.se
 Text Domain: lang_email
