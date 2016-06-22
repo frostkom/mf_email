1471183886f8bcb485317537dece353503143f02
frostkom <martin.fors@frostkom.se>
2016-06-22 11:38:20 +0200

    Change z-index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Email
 Plugin URI: https://github.com/frostkom/mf_email
 Description: 
-Version: 4.4.3
+Version: 4.4.5
 Author: Martin Fors
 Author URI: http://frostkom.se
 Text Domain: lang_email
