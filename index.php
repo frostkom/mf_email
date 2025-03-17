0f758f3fa0b0cd34e8a07cd1c9552f3944323c1d
frostkom <martin.fors@frostkom.se>
2025-03-17 12:24:02 +0100

    Remove public ajax possibility

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Email
 Plugin URI: https://github.com/frostkom/mf_email
 Description:
-Version: 6.6.27
+Version: 6.6.28
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://martinfors.se
@@ -53,7 +53,6 @@ if(!function_exists('is_plugin_active') || function_exists('is_plugin_active') &
 	add_filter('get_preferred_content_types', array($obj_email, 'get_preferred_content_types'), 10, 3);
 
 	add_action('wp_ajax_send_smtp_test', array($obj_email, 'send_smtp_test'));
-	add_action('wp_ajax_nopriv_send_smtp_test', array($obj_email, 'send_smtp_test'));
 
 	add_filter('filter_is_file_used', array($obj_email, 'filter_is_file_used'));
 
