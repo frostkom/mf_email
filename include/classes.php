b4d66036e5f6d6705b89e2cdf3c6544974553c84
frostkom <martin.fors@frostkom.se>
2025-10-11 20:00:01 +0200

    Remove old code

--- include/classes.php
+++ include/classes.php
@@ -938,19 +938,6 @@ class mf_email
 		return $new_value;
 	}
 
-	/*function pre_update_option($new_value, $old_value)
-	{
-		$out = "";
-
-		if($new_value != '')
-		{
-			$obj_encryption = new mf_encryption(__CLASS__);
-			$out = $obj_encryption->encrypt($new_value, md5(AUTH_KEY));
-		}
-
-		return $out;
-	}*/
-
 	function settings_email_callback()
 	{
 		$setting_key = get_setting_key(__FUNCTION__);
